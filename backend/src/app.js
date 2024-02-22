64b8a34e616f5a7751bb9110c29b507ca165af4c
marmariadev <marmariadev@gmail.com>
2024-02-22 18:58:04 -0300

    Add cafe routes to app

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,14 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const connectDB = require('./config/database');
+const cafeRoutes = require('./routes/cafeRoutes');
 
 const app = express();
 
 // Database connection
 connectDB();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use('/api/cafes', cafeRoutes);
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Coffee Review API');
